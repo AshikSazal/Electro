10958434db8895a410ae3ac0c698e4b07d33c31a
AshikSazal <sojolislm@gmail.com>
2023-04-04 16:10:06 +0600

    footer component add and route path set

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Error from "./components/Error/Error";
+import Support from "./pages/FooterPages/Support/Support";
+import Policy from "./pages/FooterPages/Policy/Policy";
+import Condition from "./pages/FooterPages/Condition/Condition";
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
         <Route exact path="/" element={<Home />} />
         {/* <Route path="/product/:tv" element={<Television />} /> */}
         {/* <Route path="/product/:productlist/:id" element={<Television />} /> */}
+        <Route path="/support" element={<Support />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/service" element={<Condition />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
