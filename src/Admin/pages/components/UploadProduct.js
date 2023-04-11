02433e230bebf3b17f89ce22f45420b4713a4388
AshikSazal <sojolislm@gmail.com>
2023-04-11 19:58:13 +0600

    upload product css update

--- src/Admin/pages/components/UploadProduct.js
+++ src/Admin/pages/components/UploadProduct.js
@@ -1,71 +1,79 @@
 import React from "react";
 
 import Input from "../../../shared/FormElements/Input";
-import ImageUpload from '../../../shared/FormElements/ImageUpload';
+import ImageUpload from "../../../shared/FormElements/ImageUpload";
+import Button from "../../../shared/FormElements/Button";
 import "./UploadProduct.css";
 
 const UploadProduct = () => {
   return (
     <div className="upload">
-      <div>
-      <Input
-        id="name"
-        placeholder="Name"
-        element="input"
-        type="text"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      <Input
-        id="brand"
-        placeholder="Brand"
-        element="input"
-        type="text"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      <Input
-        id="category"
-        placeholder="Category"
-        element="input"
-        type="text"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      <Input
-        id="price"
-        placeholder="Price"
-        element="input"
-        type="number"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      <Input
-        id="quantity"
-        placeholder="Quantity"
-        element="input"
-        type="number"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      <Input
-        id="name"
-        placeholder="Name"
-        element="textarea"
-        cols="40"
-        type="text"
-        errorText="Please enter a name."
-        onInput={() => {}}
-      />
-      </div>
-      <div>
-      <ImageUpload
+      <form action="">
+        <div className="input-upload">
+          <div>
+            <Input
+              id="name"
+              placeholder="Name"
+              element="input"
+              type="text"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+            <Input
+              id="brand"
+              placeholder="Brand"
+              element="input"
+              type="text"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+            <Input
+              id="category"
+              placeholder="Category"
+              element="input"
+              type="text"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+            <Input
+              id="price"
+              placeholder="Price"
+              element="input"
+              type="number"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+            <Input
+              id="quantity"
+              placeholder="Quantity"
+              element="input"
+              type="number"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+            <Input
+              id="name"
+              placeholder="Name"
+              element="textarea"
+              cols="40"
+              type="text"
+              errorText="Please enter a name."
+              onInput={() => {}}
+            />
+          </div>
+          <div>
+            <ImageUpload
               center
               id="image"
-              onInput={()=>{}}
+              onInput={() => {}}
               errorText="Please provide an image."
             />
-      </div>
+          </div>
+        </div>
+        <div>
+          <Button>Submit</Button>
+        </div>
+      </form>
     </div>
   );
 };
