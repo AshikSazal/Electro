d2b958994a3fc2a3443325a995d99f3b5acc44ad
AshikSazal <sojolislm@gmail.com>
2023-04-13 03:41:32 +0600

    Image upload functionality add

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function store(Request $req){
+        try{
+            $req->validate([
+                'name' => 'required|string|max:255',
+                'brand' => 'required|string|max:255',
+                'category' => 'required|string|max:255',
+                'price' => 'required|numeric|min:0',
+                'quantity' => 'required|integer|min:0',
+                'description' => 'required|string',
+                'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            ]);
+            $imageFile = $req->file('image');
+            $imageName = date('YmdHis') . '.' . $imageFile->getClientOriginalExtension();
+            $path = $imageFile->storeAs('public/images', $imageName);
+            $product = Product::create([
+                'name'=> $req->input('name'),
+                'brand'=> $req->input('brand'),
+                'category'=> $req->input('category'),
+                'price'=> $req->input('price'),
+                'quantity'=> $req->input('quantity'),
+                'description'=> $req->input('description'),
+                'image_path'=> $path
+            ]);
+            return response()->json(['upload' => "Image upload done"], 200);
+        }catch(Exception $exp){
+            return response(['error'=>$exp->getMessage()]);
+        }
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('check.duplication')->post('/register/{role}',[AuthController::class, 'register']);
 Route::middleware('check.login')->post('/login',[AuthController::class, 'login']);
-// Route::middleware('auth:sanctum')->post('/logout',[AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/logout',[AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->post('/product/store',[ProductController::class, 'store']);
 
 
 // Without middleware
