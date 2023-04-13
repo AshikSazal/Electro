7b3bb462592077b003be110e0f0db4d91edbfe26
AshikSazal <sojolislm@gmail.com>
2023-04-13 18:15:35 +0600

    Fetch the product in admin section and show it to the ProductList

--- src/Admin/pages/components/Product/ProductList.js
+++ src/Admin/pages/components/Product/ProductList.js
@@ -1,10 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+
+import { useHttpClient } from "../../../../shared/hooks/http-hook";
+import { useAuth } from "../../../../shared/hooks/auth-hook";
+import LoadingSpinner from "../../../../components/LoadingSpinner/LoadingSpinner";
+import ProductItem from "./ProductItem";
 
 const ProductList = () => {
+  const { token } = useAuth();
   const [product, setProduct] = useState([]);
-  return (
-    <div>ProductList</div>
-  )
-}
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-export default ProductList
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://127.0.0.1:8000/api/product/fetch",
+          "GET",
+          null,
+          {
+            Authorization: "Bearer " + token,
+          }
+        );
+        setProduct(responseData.product);
+        console.log(responseData.product);
+      } catch (err) {}
+    };
+    fetchProduct();
+  }, [setProduct, sendRequest, token]);
+
+  return (
+    <div>
+      {isLoading && <LoadingSpinner asOverlay />}
+      {product.map((prod) => (
+        <ProductItem name={prod.name} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
