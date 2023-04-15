aac39c1af0d4efb0afe759a518c8a4b920d736af
AshikSazal <sojolislm@gmail.com>
2023-04-16 01:42:15 +0600

    Without sign in add to cart will not work

--- src/product/ProductItem.js
+++ src/product/ProductItem.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 import Button from "../shared/FormElements/Button";
 import Card from "../components/Card/Card";
 import { addItemToCart } from "../store/cartSlice";
+import Modal from "../components/Error/Modal";
+import { useAuth } from "../shared/hooks/auth-hook";
 import "./ProductItem.css";
 
 const ProductItem = (props) => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { isLoggedIn } = useAuth();
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true);
+  };
+
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false);
+    navigate("/auth");
+  };
 
   const { id, category, price } = props.item;
 
@@ -21,25 +36,48 @@ const ProductItem = (props) => {
   };
 
   return (
-    <Card>
-      <div className="hov">
-        <div className="read">
-          <p>
-            <Button to={`/product/${props.item.category}/${props.item.id}`}>
-              Read More
+    <>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <React.Fragment>
+            <Button inverse onClick={cancelDeleteHandler}>
+              CANCEL
             </Button>
-          </p>
-          <p>
-            <Button onClick={addItemHandler}>Add to Cart</Button>
-          </p>
+          </React.Fragment>
+        }
+      >
+        <p className="log-in">
+          Please Login first
+        </p>
+      </Modal>
+      <Card>
+        <div className="hov">
+          <div className="read">
+            <p>
+              <Button to={`/product/${props.item.category}/${props.item.id}`}>
+                Read More
+              </Button>
+            </p>
+            <p>
+              <Button
+                onClick={isLoggedIn ? addItemHandler : showDeleteWarningHandler}
+              >
+                Add to Cart
+              </Button>
+            </p>
+          </div>
+          <div key={props.item.id}>
+            <img className="product--image" src={props.item.imageurl} alt="" />
+            <h2>{props.item.name}</h2>
+            <p className="price">{props.item.price}</p>
+          </div>
         </div>
-        <div key={props.item.id}>
-          <img className="product--image" src={props.item.imageurl} alt="" />
-          <h2>{props.item.name}</h2>
-          <p className="price">{props.item.price}</p>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </>
   );
 };
 
