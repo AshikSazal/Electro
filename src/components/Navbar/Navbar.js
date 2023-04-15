9f5a1ae5770cd2effcca8f59922c67ea72deb825
AshikSazal <sojolislm@gmail.com>
2023-04-15 20:04:42 +0600

    Big changed in Navbar for loggeout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,15 @@ import menu from "../../icons/menu.png";
 import logo from "../../icons/logo.png";
 import sign_in from "../../icons/sign_in.png";
 import HeaderCart from "../HeaderCart/HeaderCart";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./Navbar.css";
+import ErrorModal from "../Error/ErrorModal";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 const Navbar = () => {
-  const { role, logoutHandler } = useAuth();
+  const { token, role, logoutHandler } = useAuth();
   const [showNavbar, setShowNavbar] = useState(false);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
   const handleShowNavbar = () => {
     setShowNavbar(!showNavbar);
@@ -21,65 +25,80 @@ const Navbar = () => {
     }
   };
 
+  const loggedOut = async () => {
+    try {
+      await sendRequest("http://127.0.0.1:8000/api/logout", "POST", null, {
+        Authorization: "Bearer " + token + "|@|" + role,
+      });
+    } catch (err) {}
+    logoutHandler();
+  };
+
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="logo">
-          <NavLink to="/">
-            <img src={logo} alt="" />
-          </NavLink>
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      <nav className="navbar">
+        <div className="container">
+          {isLoading && <LoadingSpinner asOverlay />}
+          <div className="logo">
+            <NavLink to="/">
+              <img src={logo} alt="" />
+            </NavLink>
+          </div>
+          <div className="menu-icon" onClick={handleShowNavbar}>
+            <img src={menu} alt="" />
+          </div>
+          <div className={`nav-elements  ${showNavbar && "active"}`}>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li className="show-category">
+                <NavLink to="/product">Product</NavLink>
+                <ul className="list">
+                  <li>
+                    <NavLink to="/product/tv">Television</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/product/mobile">Mobile</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/product/laptop">Laptop</NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to="/cart">
+                  <HeaderCart />
+                </NavLink>
+              </li>
+              <li className="show-logout">
+                {+role === 3 ? (
+                  <>
+                    <img src={sign_in} alt="" />
+                    <ul className="logout">
+                      <li>
+                        <button className="btn" onClick={loggedOut}>
+                          Log out
+                        </button>
+                      </li>
+                    </ul>
+                  </>
+                ) : (
+                  <NavLink to="/auth">Login</NavLink>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="menu-icon" onClick={handleShowNavbar}>
-          <img src={menu} alt="" />
-        </div>
-        <div className={`nav-elements  ${showNavbar && "active"}`}>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li className="show-category">
-              <NavLink to="/product">Product</NavLink>
-              <ul className="list">
-                <li>
-                  <NavLink to="/product/tv">Television</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/product/mobile">Mobile</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/product/laptop">Laptop</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cart">
-                <HeaderCart />
-              </NavLink>
-            </li>
-            <li className="show-logout">
-              {+role === 3 ? (
-                <>
-                  <img src={sign_in} alt="" />
-                  <ul className="logout">
-                    <li>
-                      <button className="btn" onClick={logoutHandler}>Log out</button>
-                    </li>
-                  </ul>
-                </>
-              ) : (
-                <NavLink to="/auth">Login</NavLink>
-              )}
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
--- src/shared/hooks/auth-hook.js
+++ src/shared/hooks/auth-hook.js
@@ -1,13 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { login, logout } from "../../store/authSlice";
-import { useHttpClient } from "./http-hook";
 import { useNavigate } from "react-router-dom";
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { sendRequest } = useHttpClient();
 
   const { isLoggedIn, token, role } = useSelector((state) => state.user);
 
@@ -24,12 +22,7 @@ export const useAuth = () => {
     dispatch(login({ token, role }));
   };
 
-  const logoutHandler = async () => {
-    try {
-      await sendRequest("http://127.0.0.1:8000/api/logout", "POST",null, {
-        Authorization: "Bearer " + token+"|@|"+role,
-      });
-    } catch (err) {}
+  const logoutHandler = () => {
     dispatch(logout());
     navigate("/");
   };
