a423805a24ea0c8813177b49bdaadd4e5c73faa3
AshikSazal <sojolislm@gmail.com>
2023-04-15 18:38:00 +0600

    Fixed some css in HeaderCart

--- src/components/HeaderCart/HeaderCart.js
+++ src/components/HeaderCart/HeaderCart.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { useSelector } from "react-redux";
 
-import CartIcon from './CartIcon';
+import CartIcon from "./CartIcon";
 import "./HeaderCart.css";
 
 const HeaderCart = () => {
-    const cartQuantity = useSelector(state => state.cart.totalQuantity);
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   return (
-    <div className='cart-container'>
+    <div className="cart-container">
       <span className="icon">
         <CartIcon />
       </span>
       <span className="badge">{cartQuantity}</span>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderCart
+export default HeaderCart;
